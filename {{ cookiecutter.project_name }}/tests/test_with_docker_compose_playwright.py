7efa111c1bfc72c0e8d5d59e87e6f2ec88f87d93
Jack Linke <jacklinke@gmail.com>
2023-10-24 19:21:12 -0400

    Run docker compose fully manually

--- {{ cookiecutter.project_name }}/tests/test_with_docker_compose_playwright.py
+++ {{ cookiecutter.project_name }}/tests/test_with_docker_compose_playwright.py
@@ -2,6 +2,7 @@
 import logging
 import re
 import subprocess  # nosec
+from time import sleep
 
 import pytest
 import requests
@@ -20,40 +21,53 @@ def is_responsive(url):
         return False
 
 
+def run_docker_compose():
+    """Run docker compose using subprocess.Popen."""
+    cmd = ["docker", "compose", "up", "-d", "--build"]
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # nosec
+    stdout, stderr = proc.communicate()
+    logging.info(f"run_docker_compose return code: {proc.returncode}\noutput: {stdout}\nerrors: {stderr}")
+
+
 def check_docker_ps():
     """Check if docker compose is running, and log the output using subprocess.Popen."""
     cmd = ["docker", "ps"]
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)  # nosec
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # nosec
     stdout, stderr = proc.communicate()
     logging.info(f"check_docker_ps return code: {proc.returncode}\noutput: {stdout}\nerrors: {stderr}")
 
 
-def check_django_logs():
-    """Check if docker compose is running, and log the output using subprocess.Popen."""
+def log_django_logs():
+    """Log the django logs using subprocess.Popen."""
     cmd = ["docker", "compose", "logs", "django_test"]
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)  # nosec
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # nosec
     stdout, stderr = proc.communicate()
-    logging.info(f"check_docker_ps return code: {proc.returncode}\noutput: {stdout}\nerrors: {stderr}")
+    logging.info(f"log_django_logs return code: {proc.returncode}\noutput: {stdout}\nerrors: {stderr}")
 
 
-def check_postgres_logs():
-    """Check if docker compose is running, and log the output using subprocess.Popen."""
+def log_postgres_logs():
+    """Log the postgres logs using subprocess.Popen."""
     cmd = ["docker", "compose", "logs", "postgres_test"]
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)  # nosec
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # nosec
     stdout, stderr = proc.communicate()
-    logging.info(f"check_docker_ps return code: {proc.returncode}\noutput: {stdout}\nerrors: {stderr}")
+    logging.info(f"log_postgres_logs return code: {proc.returncode}\noutput: {stdout}\nerrors: {stderr}")
 
 
 @pytest.fixture(scope="session")
 def http_service(docker_ip, docker_services):
     """Ensure that HTTP service is up and responsive."""
-    port = docker_services.port_for("django_test", 8000)
+    # port = docker_services.port_for("django_test", 8000)
     # url = f"http://{docker_ip}:{port}/"  # Maybe 0.0.0.0?
-    url = f"http://0.0.0.0:{port}/"
+    url = "http://0.0.0.0:8111/"
     logging.info(f"http_service url: {url}")
+    run_docker_compose()
+
+    # Pause 30 seconds to allow the service to start up.
+    sleep(30)
+
     check_docker_ps()
-    check_django_logs()
-    check_postgres_logs()
+    log_django_logs()
+    log_postgres_logs()
     # docker_services.wait_until_responsive(timeout=60.0, pause=0.1, check=lambda: is_responsive(url))
 
     return url
@@ -61,15 +75,25 @@ def http_service(docker_ip, docker_services):
 
 def test_status_code(http_service):
     """Check the status code of the HTTP service."""
-    response = requests.get(http_service, timeout=60)
-    logging.info(f"test_status_code response: {response}")
+    try:
+        response = requests.get(http_service, timeout=60)
+        logging.info(f"test_status_code response: {response}")
 
-    assert response.status_code == 200
+        assert response.status_code == 200
+    except ConnectionError as e:
+        logging.info("ConnectionError")
+        log_django_logs()
+        raise AssertionError() from e
 
 
 def test_has_corrrect_title(http_service, page: Page):
     """Test that the page has the correct title."""
-    page.goto(http_service)
+    try:
+        page.goto(http_service)
 
-    # Expect a title "to contain" a substring.
-    expect(page).to_have_title(re.compile("The install worked successfully! Congratulations!"))
+        # Expect a title "to contain" a substring.
+        expect(page).to_have_title(re.compile("The install worked successfully! Congratulations!"))
+    except ConnectionError as e:
+        logging.info("ConnectionError")
+        log_django_logs()
+        raise AssertionError() from e
